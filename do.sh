1b47459e5c18ab407b653a261376350b4907a202
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2020-12-28 15:41:01 +0000

    More

--- do.sh
+++ do.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set -eoux pipefail
+rm -f slog slog_html
+
 ./x.py b src/librustdoc library/std                
 #rustdoc +stage1 --edition 2018 --output-format json src/test/rustdoc-json/nested.rs -o . 2> log_raw
 #python3 tree.py
 cd ../regex
+rm -rf target/
 cargo +stage1 rustdoc -v -- --output-format json 2>&1 | rg "START|END" > ../rust/tree
 mv target/doc/regex.json ../rust/nested.json
 cd ../rust
